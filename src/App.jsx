6264d16df5bdb4f7b564eaccf1ea272fe4462c15
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-21 23:45:11 +0200

    Add Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-import Toggle from './components/Toggle'
+import Toggle from './components/Toggle';
+import Button from './components/Button';
 
 function App() {
   
 
   return (
     <div className='to_remove'>
+      <Button buttonClass='button-primery' nameButton='Apply now'/>
       <Toggle />
     </div>
   )
