ab6edcf5cdbf45bec0e5a667f098dacedef4cb40
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-21 15:25:05 +0200

    css reset, root element and basic button styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ function App() {
   
 
   return (
-    <>
-     
-    </>
+    <div className='to_remove'>
+      <div><button className='button button-primery'>Button 1</button></div>
+      <div><button className='button button-secondary'>Button 2</button></div>
+    </div>
   )
 }
 
