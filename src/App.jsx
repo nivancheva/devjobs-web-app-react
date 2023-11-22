96bba520736c47bf98b1056f3d3898a55e0e8f55
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-22 11:17:57 +0200

    import data from data.json file

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import Toggle from './components/Toggle';
 import Button from './components/Button';
 import Checkbox from './components/Checkbox';
 import Card from './components/Card';
+import dataJson from './data.json';
+import { useState } from 'react';
 
 function App() {
-  
+  const [cards] = useState(dataJson);
 
   return (
     <div className='to_remove'>
       <Button buttonClass='button-primery' nameButton='Apply now'/>
       <Toggle />
       <Checkbox />
-      <Card />
+      <div className='cards-container'>
+          {cards.map((card, idx) => {
+            return (
+              <Card key={idx} {...card}/>
+            )
+          })}
+        </div>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,18 @@
 import './Card.css';
-import scoot from '../assets/logos/scoot.svg'
 
-export default function Card() {
+export default function Card({ logo, postedAt, contract, position, company, location }) {
     return (
         <div className='card-container'>
             <div className='logo-wrapper'>
-                <img className='logo' src={scoot}/>
+                <img className='logo' src={logo}/>
             </div>
             <div>
                 <div>
-                    <p className='post-aded'> 5h ago <span className='contract-type'>Full Time</span></p>
-                    <h3 className='position'>Senior Software Engineer</h3>
-                    <p className='company-name'>Scoot</p>
+                    <p className='post-aded'>{postedAt} <span className='contract-type'>{contract}</span></p>
+                    <h3 className='position'>{position}</h3>
+                    <p className='company-name'>{company}</p>
                 </div>
-                <p className='company-location'>United Kingdom</p>
+                <p className='company-location'>{location}</p>
             </div>
         </div>
     )
