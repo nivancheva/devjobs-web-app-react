7704400e6e1628153ebf7f79989300d131c77367
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-22 16:55:59 +0200

    center main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import './App.css'
-
-import Button from './components/Button';
-import Checkbox from './components/Checkbox';
 import Card from './components/Card';
 import dataJson from './data.json';
 import { useState } from 'react';
 import Header from './components/Header';
+import Inputs from './components/Inputs';
 
 function App() {
   const [cards] = useState(dataJson);
 
   return (
     <div>
-      <Header />
-      {/* <Button primary>Apply now</Button> */}
+      <div className='header-input'>
+        <Header />
       
-      {/* <Checkbox /> */}
+        <Inputs />
+      </div>
+      
+      
       <div className='container cards-container'>
           {cards.map((card, idx) => {
             return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,11 @@ import logo from '../assets/images/logo.svg'
 
 export default function Header() {
     return (
-        <div className='flex header-wrapper'>
-            <img src={logo}/>
-            <Toggle />
+        <div className='header-wrapper'>
+            <div className='flex container header'>
+                <img src={logo}/>
+                <Toggle />
+            </div>
         </div>
     )
 }
