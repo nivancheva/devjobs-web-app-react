ddbb091ee5881c6a922b419c04faee628e900d9e
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-23 15:50:50 +0200

    fix button pading and li element color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, primary, onClick }) {
     return (
         <div>
-            <button onClick={onClick} className={`button ${primary ? "button-primary" : "button-secondary"}`}>{children}</button>
+            <button onClick={onClick} className={`mobile-button button ${primary ? "button-primary" : "button-secondary"}`}>{children}</button>
         </div>
     )   
 }
