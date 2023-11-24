63ff66fdcce9346c4a2ccfa06e2b77cd60278419
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-24 09:09:28 +0200

    add job description footer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,11 @@
 export default function Button({ children, primary, onClick, href }) {
     return (
-        <div>
+        <>
             { href ?
                 <a href={href} target="_blank" className={`button ${primary ? "button-primary" : "button-secondary"}`}>{children}</a>
                 :
                 <button onClick={onClick} className={`button ${primary ? "button-primary" : "button-secondary"}`}>{children}</button>
             }
-            
-        </div>
+        </>
     )   
 }
