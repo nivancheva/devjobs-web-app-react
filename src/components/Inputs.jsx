81ac27ff6322d5277f5a3cc719a5ffee2b1a9541
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-28 12:16:22 +0200

    add filter on mobile

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -38,16 +38,16 @@ export default function Inputs({ onSearch }) {
 
             <div className='inputs-wrapper-mobile'>
                 <div className='inputs-mobile'>
-                    <Input reverse image={iconFilter} placeholder='Filter by title…'/>   
+                    <Input reverse value={position} onChange={setPosition} image={iconFilter} placeholder='Filter by title…'/>   
                     <button className='icon-search-button' onClick={showFilterBox}><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M17.112 15.059h-1.088l-.377-.377a8.814 8.814 0 002.15-5.784A8.898 8.898 0 008.898 0 8.898 8.898 0 000 8.898a8.898 8.898 0 008.898 8.899c2.211 0 4.23-.808 5.784-2.143l.377.377v1.081l6.845 6.832 2.04-2.04-6.832-6.845zm-8.214 0A6.16 6.16 0 118.9 2.737a6.16 6.16 0 010 12.322z" fill="#FFFFFF" fillRule="nonzero"/></svg></button>
                 </div>
             
                 {modalVisible && 
                     <div className='overlay'>
                         <div className='filter-box'>
-                            <Input image={locationIcon} placeholder='Filter by location…'/>
+                            <Input value={location} onChange={setLocation} image={locationIcon} placeholder='Filter by location…'/>
                             <div className='checkbox-mobile'>
-                                <Checkbox />
+                                <Checkbox value={fullTime} onChange={setFullTime}/>
                                 <p>Full Time Only</p>
                             
                                 <Button onClick={onSearchButtonClick} primary>Search</Button>
